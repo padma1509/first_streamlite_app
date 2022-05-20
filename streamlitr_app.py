c3db5e6ca23337b743aee3d3845054d2936c7ad2
padma1509 <48042774+padma1509@users.noreply.github.com>
2022-05-20 13:17:33 +0530

    Update streamlitr_app.py

--- streamlitr_app.py
+++ streamlitr_app.py
@@ -1,10 +1,11 @@
 import streamlit
-streamlit.title('My Parents New Dinner List')
+streamlit.title('My Mom''s New Healthy Dinner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avocado toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
